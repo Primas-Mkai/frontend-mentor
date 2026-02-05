ebf6da2ec51fd52ac2a650bf4759eeb5739515a4
Primas-Mkai <mkaiprimas1@gmail.com>
2026-02-04 18:24:23 -0800

    Update index.html, style.css and add main.js files

---
+++ article-preview-component-master/main.js
@@ -0,0 +1,9 @@
+
+
+document.querySelector(".icon-holder").addEventListener("click", function(){
+    document.querySelector(".popup-share-container").style.display = "block";
+});
+
+document.querySelector(".share-icon-holder").addEventListener("click", function(){
+    document.querySelector(".popup-share-container").style.display = "none";
+});
