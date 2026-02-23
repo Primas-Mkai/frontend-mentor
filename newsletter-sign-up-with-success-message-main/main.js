cd2d6e2f59f0f19529413b7d92313c58a454b5e5
Primas-mkai <mkaiprimas1@gmail.com>
2026-02-23 18:38:01 +0300

    Update index.html, style.css and main.js files

--- newsletter-sign-up-with-success-message-main/main.js
+++ newsletter-sign-up-with-success-message-main/main.js
@@ -1,21 +1,49 @@
 
-// prevent default
-const form = document.getElementById("form");
 
-function handleSubmit(e) {
-    e.preventDefault();
+const form = document.querySelector("form");
+const email = document.getElementById("email");
+const button = document.querySelector("button");
+const invalidInput = document.querySelector("input:invalid");
+const emailError = document.querySelector("span");
+
+
+button.addEventListener('click', (event) => {
+    if(email.validity.valid) {
+        emailError.textContent = "";
+        emailError.className = "error";
+    } else {
+        showError();
+        email.classList.add("errorMail");
+    }
+})
+
+form.addEventListener('submit', (event) => {
+    if(!email.validity.valid) {
+        showError();
+        email.classList.add("errorMail");
+
+        event.preventDefault(); // prevent default
+    }
+})
+
+function showError() {
+    if(email.validity.valueMissing) {
+        emailError.textContent = "You need to enter an email";
+    } else if(email.validity.typeMismatch) {
+        emailError.textContent = "Valid email required";
+    }
+
+    emailError.className = "error active";
 }
 
-form.addEventListener('submit', handleSubmit);
 
-const button = document.querySelector("button");
-const errorText = document.querySelector("#error");
-const inputEmail = document.querySelector("#email");
-
-errorText.classList.add("hide"); 
-inputEmail.classList.remove("displayError");
-
-button.addEventListener('click', function(e) {
-    errorText.classList.toggle("hide");
-    inputEmail.classList.toggle("displayError");
-})
+email.addEventListener('focus', (event) => {
+    if(email.classList.contains("errorMail")) {
+        emailError.textContent = "";
+        email.addEventListener('keydown', (event) => {
+            if(event.key === 'Backspace' || event.key === 'Delete') {
+                email.classList.remove("errorMail");
+            }
+        });
+    }
+});
