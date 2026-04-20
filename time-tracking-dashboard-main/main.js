33455543dc3af2ec786ec6af62772e58b69b9a4a
Primas Mkai <mkaiprimas1@gmail.com>
2026-04-20 15:53:15 +0300

    Final commit

--- time-tracking-dashboard-main/main.js
+++ time-tracking-dashboard-main/main.js
@@ -3,34 +3,91 @@ const weekly = document.querySelector("#weekly")
 const monthly = document.querySelector("#monthly");
 
 daily.addEventListener("click", () => {
-    updateDailyContent();
+    updateDailyContent(json);
 })
 
 weekly.addEventListener("click", () => {
-    alert("You clicked weekly");
+    updateWeeklyContent(json);
 })
 
 monthly.addEventListener("click", () => {
-    alert("You clicked monthly");
+    updateMonthlyContent(json);
 })
+
+let json;
 
 fetch('data.json').then((response) => {
     if(!response.ok) return console.log("Oops, something went wrong");
 
     return response.json();
 }).then((data) => {
-    console.log(data[0].timeframes.daily.current);
-    updateDailyContent(data);
+    json = data;
+    updateWeeklyContent(json);
 }).catch((error) => {
     alert(`Could not fetch data: ${error}`)
 });
 
+const workCurrentHours = document.querySelector("#work-current");
+const playCurrentHours = document.querySelector("#play-current");
+const studyCurrentHours = document.querySelector("#study-current");
+const exerciseCurrentHours = document.querySelector("#exercise-current");
+const socialCurrentHours = document.querySelector("#social-current");
+const selfCareCurrentHours = document.querySelector("#self-care-current");
 
-function updateDailyContent(obj) {
-    const workCurrentHours = document.querySelector("#work-current");
+const workPreviousHours = document.querySelector("#work-previous");
+const playPreviousHours = document.querySelector("#play-previous");
+const studyPreviousHours = document.querySelector("#study-previous");
+const exercisePreviousHours = document.querySelector("#exercise-previous");
+const socialPreviousHours = document.querySelector("#social-previous");
+const selfCarePreviousHours = document.querySelector("#self-care-previous");
 
-    workCurrentHours.textContent = `${obj[0].timeframes.daily.current}hrs`;
-}
+function updateDailyContent(obj1) {
+    workCurrentHours.textContent = `${obj1[0].timeframes.daily.current}hrs`;
+    playCurrentHours.textContent = `${obj1[1].timeframes.daily.current}hrs`;
+    studyCurrentHours.textContent = `${obj1[2].timeframes.daily.current}hrs`;
+    exerciseCurrentHours.textContent = `${obj1[3].timeframes.daily.current}hrs`;
+    socialCurrentHours.textContent = `${obj1[4].timeframes.daily.current}hrs`;
+    selfCareCurrentHours.textContent = `${obj1[5].timeframes.daily.current}hrs`;
+
+    workPreviousHours.textContent = `Last day - ${obj1[0].timeframes.daily.previous}hrs`;
+    playPreviousHours.textContent = `Last day - ${obj1[1].timeframes.daily.previous}hrs`;
+    studyPreviousHours.textContent = `Last day - ${obj1[2].timeframes.daily.previous}hrs`;
+    exercisePreviousHours.textContent = `Last day - ${obj1[3].timeframes.daily.previous}hrs`;
+    socialPreviousHours.textContent = `Last day - ${obj1[4].timeframes.daily.previous}hrs`;
+    selfCarePreviousHours.textContent = `Last day - ${obj1[5].timeframes.daily.previous}hrs`; 
+};
+
+function updateWeeklyContent(obj2) {
+    workCurrentHours.textContent = `${obj2[0].timeframes.weekly.current}hrs`;
+    playCurrentHours.textContent = `${obj2[1].timeframes.weekly.current}hrs`;
+    studyCurrentHours.textContent = `${obj2[2].timeframes.weekly.current}hrs`;
+    exerciseCurrentHours.textContent = `${obj2[3].timeframes.weekly.current}hrs`;
+    socialCurrentHours.textContent = `${obj2[4].timeframes.weekly.current}hrs`;
+    selfCareCurrentHours.textContent = `${obj2[5].timeframes.weekly.current}hrs`;
+
+    workPreviousHours.textContent = `Last week - ${obj2[0].timeframes.weekly.previous}hrs`;
+    playPreviousHours.textContent = `Last week - ${obj2[1].timeframes.weekly.previous}hrs`;
+    studyPreviousHours.textContent = `Last week - ${obj2[2].timeframes.weekly.previous}hrs`;
+    exercisePreviousHours.textContent = `Last week - ${obj2[3].timeframes.weekly.previous}hrs`;
+    socialPreviousHours.textContent = `Last week - ${obj2[4].timeframes.weekly.previous}hrs`;
+    selfCarePreviousHours.textContent = `Last week - ${obj2[5].timeframes.weekly.previous}hrs`;
+};
+
+function updateMonthlyContent(obj3) {
+    workCurrentHours.textContent = `${obj3[0].timeframes.monthly.current}hrs`;
+    playCurrentHours.textContent = `${obj3[1].timeframes.monthly.current}hrs`;
+    studyCurrentHours.textContent = `${obj3[2].timeframes.monthly.current}hrs`;
+    exerciseCurrentHours.textContent = `${obj3[3].timeframes.monthly.current}hrs`;
+    socialCurrentHours.textContent = `${obj3[4].timeframes.monthly.current}hrs`;
+    selfCareCurrentHours.textContent = `${obj3[5].timeframes.monthly.current}hrs`;
+
+    workPreviousHours.textContent = `Last month - ${obj3[0].timeframes.monthly.previous}hrs`;
+    playPreviousHours.textContent = `Last month - ${obj3[1].timeframes.monthly.previous}hrs`;
+    studyPreviousHours.textContent = `Last month - ${obj3[2].timeframes.monthly.previous}hrs`;
+    exercisePreviousHours.textContent = `Last month - ${obj3[3].timeframes.monthly.previous}hrs`;
+    socialPreviousHours.textContent = `Last month - ${obj3[4].timeframes.monthly.previous}hrs`;
+    selfCarePreviousHours.textContent = `Last month - ${obj3[5].timeframes.monthly.previous}hrs`;
+};
 
 
 /* the data from json file will replace these data 
